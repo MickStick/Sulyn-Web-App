15863c689fa9806ff78c704bb294a6fc575089a7
Mikhail R. Shaw <mickstick01@hotmail.com>
2017-10-23 18:05:42 -0500

    added new fields in the reservation form, set the correct added new attributes, according to new fields, to reservation to be saved

--- public/js/script.js
+++ public/js/script.js
@@ -1,80 +1,79 @@
-$(document).ready(function(){
+$(document).ready(function() {
     console.log(window.location);
-    const users = [
-        {
+    const users = [{
             name: "Mikhail Shaw",
             password: "pass4040"
-        },                                     ////////////////////////////
-        {                                      //
-            name: "Wembley Williams",          // Hardcoded hostess login
-            password: "TheLeo"                 // information
-        },                                     //
-        {                                      /////////////////////////////
+        }, ////////////////////////////
+        { //
+            name: "Wembley Williams", // Hardcoded hostess login
+            password: "TheLeo" // information
+        }, //
+        { /////////////////////////////
             name: "David Rhoden",
             password: "pavt3c"
         }
     ];
 
 
-    checkStat= () => {
-        if(localStorage.getItem("status") != undefined){
-            $('.host-route').css({"display":"initial"});
-            $('.log-route').css({"display":"none"});        ///////////////////////////////////////
-            return true;                                    //  
-        }else{                                              // This displays and hides links 
-            $('.host-route').css({"display":"none"});       // that should be showed to a loogged
-           $('.log-route').css({"display":"initial"});      // in hostess or a guest
-            return false;                                   //
-        }                                                   ////////////////////////////////////////
+    checkStat = () => {
+        if (localStorage.getItem("status") != undefined) {
+            $('.host-route').css({ "display": "initial" });
+            $('.log-route').css({ "display": "none" }); ///////////////////////////////////////
+            return true; //  
+        } else { // This displays and hides links 
+            $('.host-route').css({ "display": "none" }); // that should be showed to a loogged
+            $('.log-route').css({ "display": "initial" }); // in hostess or a guest
+            return false; //
+        } ////////////////////////////////////////
     }
 
     const views = $('.view'); // Retreives all views and stores them in an array
     setHidden = () => {
-            var x = 0;                                  ////////////////////////////////////
-            for(x = 0; x < views.length; x++){          // Hides all views for navigation
-                views.eq(x).attr("hidden", "true");     ///////////////////////////////////
-            }
+        var x = 0; ////////////////////////////////////
+        for (x = 0; x < views.length; x++) { // Hides all views for navigation
+            views.eq(x).attr("hidden", "true"); ///////////////////////////////////
+        }
     }
-        
+
     checkView = () => {
-        if(window.location.pathname == "/"){
+        if (window.location.pathname == "/") {
             setHidden();
             getView("/home");
-        }else if(window.location.pathname == "/hostess"){
-            if(localStorage.getItem("status") != undefined){
+        } else if (window.location.pathname == "/hostess") {
+            if (localStorage.getItem("status") != undefined) {
                 getView(window.location.pathname);
-            }else{
-                window.location.pathname = "/login";                ///////////////////////////////////     
-            }                                                       //
-        }else if(window.location.pathname == "/login"){             // On page load, check the url and
-            if(!checkStat()){                                       // display the view that 
-                getView("/login");                                  // corresponds with the url
-            }else{                                                  //
-                window.location.pathname = "/hostess";              //////////////////////////////////
+            } else {
+                window.location.pathname = "/login"; ///////////////////////////////////     
+            } //
+        } else if (window.location.pathname == "/login") { // On page load, check the url and
+            if (!checkStat()) { // display the view that 
+                getView("/login"); // corresponds with the url
+            } else { //
+                window.location.pathname = "/hostess"; //////////////////////////////////
                 getView("/hostess");
             }
-        }else if(window.location.pathname == "/logout"){
+        } else if (window.location.pathname == "/logout") {
             onLogout();
-        }else{
+        } else {
             getView(window.location.pathname);
         }
     }
 
     function getView(path) {
-        view = $(''+path.replace("/","#")+'');
+        view = $('' + path.replace("/", "#") + '');
         view.removeAttr("hidden");
 
-        if(view == "#resConfirm" || view == "#reservation"){
-            
-        }else{
-            $('.route-item').css({"background":"transparent"}); // Sets active link
-            $('a[href="'+path.replace("/","#")+'"]').parent('li').css({"background":"rgba(20,180,90,0.5)"});
+        if (view == "#resConfirm" || view == "#reservation") {
+
+        } else {
+            $('.route-item').css({ "background": "transparent" }); // Sets active link
+            $('a[href="' + path.replace("/", "#") + '"]').parent('li').css({ "background": "rgba(20,180,90,0.5)" });
         }
 
-        if(path != "/home"){ // Ensures that url path is null when on home
-            window.history.pushState(null,null,window.location.origin + path);// Changes the url without reload
-        }else{
-            window.history.pushState(null,null,window.location.origin);// Changes the url without reload
+        if (path != "/home") { // Ensures that url path is null when on home
+            window.history.pushState(null, null, window.location.origin + path); // Changes the url without reload
+        } else {
+            window.history.pushState(null, null, window.location.origin); // Changes the url without reload
         }
         console.log("Path: ", path);
         return false;
@@ -82,102 +81,133 @@ $(document).ready(function(){
 
     onLogout = () => {
         localStorage.clear();
-        $('.hostess #hostess-name').text("");   ////////////////////////////////////////
-        setHidden();                            // Logs out hostess then redirects to 
-        getView("/login");                      // login view
-        checkStat();                            ////////////////////////////////////////
+        $('.hostess #hostess-name').text(""); ////////////////////////////////////////
+        setHidden(); // Logs out hostess then redirects to 
+        getView("/login"); // login view
+        checkStat(); ////////////////////////////////////////
     }
 
-    function isValid(name, pword){ // Login validation
-        for(x = 0; x < users.length; x++){
-            if(name == users[x].name && pword == users[x].password){
+    function isValid(name, pword) { // Login validation
+        for (x = 0; x < users.length; x++) {
+            if (name == users[x].name && pword == users[x].password) {
                 return true;
             }
         }
         return false;
     }
 
-    checkStat();    // Thease are run onLoad
-    checkView();    //
+    checkStat(); // Thease are run onLoad
+    checkView(); //
 
-    $('.route').click(function(e){
+    $('.route').click(function(e) {
         e.preventDefault();
-        if($(this).attr("href") == "/logout"){
+        if ($(this).attr("href") == "/logout") {
             onLogout();
-        }else {
-                setHidden();
-                getView($(this).attr("href"));
+        } else {
+            setHidden();
+            getView($(this).attr("href"));
         }
     });
 
-    $('#login-btn').click(function(e){
+    $('#login-btn').click(function(e) {
         e.preventDefault();
-        if(isValid($('.login-form input[name="uname"]').val(),$('.login-form input[name="pword"]').val())){
+        if (isValid($('.login-form input[name="uname"]').val(), $('.login-form input[name="pword"]').val())) {
             $('.login-form #loginErr').text("");
-            localStorage.setItem("status","logged");
-            localStorage.setItem("name",$('.login-form input[name="uname"]').val());
+            localStorage.setItem("status", "logged");
+            localStorage.setItem("name", $('.login-form input[name="uname"]').val());
             setHidden();
             $('#hostess-name').text("Welcome " + localStorage.getItem("name"));
             $('.login-form input').val("");
             getView("/hostess");
-            checkStat();      
-        }else{
+            checkStat();
+        } else {
             $('.login-form #loginErr').text("Invalid Login Credentials");
         }
-        
+
     });
 
-    $('#guest-btn').click(function(e){
+    $('#guest-btn').click(function(e) {
         e.preventDefault();
         var reserve = { // Stores reservation form information in a JSON object
-            name:$('.guest-form input[name="name"]').val(), 
-            time:$('.guest-form input[name="time"]').val(),
-            date:$('.guest-form input[name="date"]').val(),
-            seat:$('.guest-form input[name="seat"]').val()
+            name: $('.guest-form input[name="name"]').val(),
+            rNum: $('.guest-form input[name="rNum"]').val(),
+            gNum: $('.guest-form input[name="gNum"]').val(),
+            restaurant: $('.guest-form select[name="restaurants"]').val(),
+            time: $('.guest-form input[name="time"]').val(),
+            date: $('.guest-form input[name="date"]').val(),
+            seat: $('.guest-form input[name="seat"]').val(),
+            email: $('.guest-form input[name="email"]').val()
         };
         $.ajax({
-            type: 'POST',               ////////////////////////////////////////////
-            url: '/reserve',            // Send info to server to be processed
-            data: reserve,              ///////////////////////////////////////////
-            success: function(data){
+            type: 'POST', ////////////////////////////////////////////
+            url: '/reserve', // Send info to server to be processed
+            data: reserve, ///////////////////////////////////////////
+            success: function(data) {
                 setHidden();
-                $('.resConfirm-table #resNum').text(data.ticket);       ///////////////////////////////////////
-                $('.resConfirm-table #resName').text(reserve.name);     //
-                $('.resConfirm-table #resTime').text(reserve.time);     // Receives response containing data.
-                $('.resConfirm-table #resDate').text(reserve.date);     // Sets confirmation fields.
-                $('.resConfirm-table #resSeat').text(reserve.seat);     // Calls confirmation view.
-                getView("#resConfirm");                                 // Sets reservation inputs to null. 
-                $('.guest-form input').val("");                         /////////////////////////////////////
+                $('.resConfirm-table #resNum').text(data.ticket);
+                $('.resConfirm-table #resName').text(reserve.name);
+                $('.resConfirm-table #resrNum').text(reserve.rNum);
+                $('.resConfirm-table #resgNum').text(reserve.gNum); ///////////////////////////////////////
+                $('.resConfirm-table #resRest').text(reserve.restaurant); //
+                $('.resConfirm-table #resTime').text(reserve.time); // Receives response containing data.
+                $('.resConfirm-table #resDate').text(reserve.date); // Sets confirmation fields.
+                $('.resConfirm-table #resSeat').text(reserve.seat); //
+                $('.resConfirm-table #resEmail').text(reserve.email); // Calls confirmation view.
+                getView("#resConfirm"); // Sets reservation inputs to null. 
+                $('.guest-form input').val(""); /////////////////////////////////////
                 console.log(data);
             }
         });
     });
 
-    $('#host-btn').click(function(e){
+    $('#host-btn').click(function(e) {
         e.preventDefault();
         var reserve = { // Stroes info from retreive table in a JSON object
-            name:$('.hostess-form input[name="name"]').val(),      
-            ticket:$('.hostess-form input[name="ticket"]').val()
+            name: $('.hostess-form input[name="name"]').val(),
+            ticket: $('.hostess-form input[name="ticket"]').val()
         };
         $.ajax({
-            type: 'POST',               ///////////////////////////////////////////
-            url: '/getreserve',         // Send info to server to be processed
-            data: reserve,              ///////////////////////////////////////////
-            success: function(data){
-                if(data.success){
+            type: 'POST', ///////////////////////////////////////////
+            url: '/getreserve', // Send info to server to be processed
+            data: reserve, ///////////////////////////////////////////
+            success: function(data) {
+                if (data.success) {
                     console.log(data);
                     setHidden();
-                    $('.reserve-table #resName').text(data.reservation.name);       ///////////////////////////////////////
-                    $('.reserve-table #resTime').text(data.reservation.time);       //
-                    $('.reserve-table #resDate').text(data.reservation.date);       // Receives response containing data.
-                    $('.reserve-table #resSeat').text(data.reservation.seat);       // Sets retreived fields.
-                    getView("#reservation");                                        // Calls retreived  view.
-                    $('.hostess-form #resErr').text("");                            // Sets retireive  inputs to null.
-                    $('.hostess-form input').val("");                               // Sets error message to null
-                }else {                                                             //        
-                    $('.hostess-form #resErr').text(data.msg);                      /////////////////////////////////////
-                }                
+                    $('.reserve-table #resName').text(data.reservation.name); ///////////////////////////////////////
+                    $('.reserve-table #resTime').text(data.reservation.time); //
+                    $('.reserve-table #resDate').text(data.reservation.date); // Receives response containing data.
+                    $('.reserve-table #resSeat').text(data.reservation.seat); // Sets retreived fields.
+                    getView("#reservation"); // Calls retreived  view.
+                    $('.hostess-form #resErr').text(""); // Sets retireive  inputs to null.
+                    $('.hostess-form input').val(""); // Sets error message to null
+                } else { //        
+                    $('.hostess-form #resErr').text(data.msg); /////////////////////////////////////
+                }
             }
         });
     });
+
+    $('.guest-form input[name="date"]').blur(function() {
+        console.log("date blurred");
+        var date = $(this).val().split('-');
+        for (x = 0; x < date.length; x++) {
+            console.log(date[x]);
+        }
+
+    });
+
+    $('.guest-form input[name="time"]').blur(function() {
+        console.log("time blurred");
+        var time = $(this).val().split(':');
+        for (x = 0; x < time.length; x++) {
+            console.log(time[x]);
+        }
+
+    });
+
+    $('.guest-form select[name="restaurants"]').blur(function() {
+        console.log($(this).val());
+    });
+
 });
--- server.js
+++ server.js
@@ -8,60 +8,67 @@ const port = 8888;
 const app = express();
 
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-app.post('/reserve', urlcp , function(req,res,next){
+app.post('/reserve', urlcp, function(req, res, next) {
     var reservations = null;
     var reserveSet = {};
-    fs.readFile('public/js/reserve.json', function(err,data){
-        if(err){
+    fs.readFile('public/js/reserve.json', function(err, data) {
+        if (err) {
             console.log(err);
-            res.json({success: false});
+            res.json({ success: false });
         }
 
-        reservations = JSON.parse(data); 
+        reservations = JSON.parse(data);
         //console.log(reservations);
         reserveSet = {
             name: req.body.name,
+            gNum: req.body.gNum,
+            rNum: req.body.rNum,
+            restaurant: req.body.restaurant,
             time: req.body.time,
             date: req.body.date,
-            seat: req.body.seat
+            seat: req.body.seat,
+            email: req.body.email
         }
-        if(reservations[0]!= undefined){
+        if (reservations[0] != undefined) {
             reserveSet.reservationNum = reservations[reservations.length - 1].reservationNum + 1;
-        }else{
+        } else {
             reserveSet.reservationNum = 1000;
         }
         reservations.push(reserveSet);
         var DATA = JSON.stringify(reservations);
-        DATA = DATA.split(",");
-        console.log(DATA);
-        fs.writeFile('public/js/reserve.json', DATA, function(err){
-            if(err){
+        Data = DATA.split(",");
+        console.log(Data);
+        fs.writeFile('public/js/reserve.json', Data, function(err) {
+            if (err) {
                 console.log(err);
             }
         });
-        res.json({success:"supmn",ticket:reserveSet.reservationNum});
+        res.json({ success: "supmn", ticket: reserveSet.reservationNum });
     });
 });
 
-app.post('/getreserve', urlcp, function(req, res, next){
+app.post('/getreserve', urlcp, function(req, res, next) {
     console.log("getreserve");
-    fs.readFile('public/js/reserve.json', function(err,data){
-        if(err){
+    fs.readFile('public/js/reserve.json', function(err, data) {
+        if (err) {
             console.log(err);
-            res.json({success: false, msg: "File Error"});
+            res.json({ success: false, msg: "File Error" });
         }
         console.log(req.body);
         chk = findReservation(req.body, JSON.parse(data));
-        if(!chk){
+        if (!chk) {
             res.json({
-                success: false, 
+                success: false,
                 msg: "Reservation Not Found"
             });
-        }else{
+        } else {
             res.json({
-                success: true, 
+                success: true,
                 msg: "found",
                 reservation: chk
             });
@@ -69,21 +76,53 @@ app.post('/getreserve', urlcp, function(req, res, next){
     });
 });
 
-app.get('*', function(req,res, next){
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.get('*', function(req, res, next) {
+    res.render('index', { name: "Solar Flare" });
+    //res.sendFile(path.join(__dirname, '/index.html', {name: "Solar Flare"}));
 });
 
 
 
-function findReservation(Res, data){
-    for(x = 0; x < data.length; x++){
-        if(Res.name == data[x].name && Res.ticket == parseInt(data[x].reservationNum)){
+function findReservation(Res, data) {
+    for (x = 0; x < data.length; x++) {
+        if (Res.name == data[x].name && Res.ticket == parseInt(data[x].reservationNum)) {
             return data[x];
         }
     }
     return false;
 }
 
-app.listen(port, function(){
+var nodemailer = require('nodemailer');
+var schedule = require('node-schedule');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'mikshaw01@gmail.com',
+        pass: 'Cornwall01'
+    }
+});
+
+var mailOptions = {
+    from: 'mikshaw01@gmail.com',
+    to: 'mickstick01@hotmail.com',
+    subject: 'Testing th NodeJS Mail in a server',
+    text: 'That was easy!',
+    html: '<h1> Check this out!! </h1> <br> <a href="http://localhost:8888/guest">Click me</a>'
+};
+
+
+var j = schedule.scheduleJob({ hour: 12, minute: 35, dayOfWeek: 6 }, function() {
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+});
+
+
+app.listen(port, function() {
     console.log("Server listening to: " + port);
-}); 
+});
